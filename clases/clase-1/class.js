dfa76dca5e7b2a72663e24c2cadf73d86a3326ea
Jose Quezada <jose.quezada@claro.com.ar>
2023-07-23 15:22:31 -0300

    Clases - Actualizacion de archivos

--- clases/clase-1/class.js
+++ clases/clase-1/class.js
@@ -1,6 +1,8 @@
 class Persona {
-    constructor(nombre) {
+    #cuit;
+    constructor(nombre, cuit) {
         this.nombre = nombre;
+        this.#cuit = cuit;
     }
 
     static especie = 'Humano';
@@ -11,16 +13,26 @@ class Persona {
     saludar = () => {
         console.log(`Hola soy ${this.nombre}`)
     }
+
+    getCuit = () => {
+        console.log(this.#cuit);
+    }
 }
 
 //creo las instancias
-const persona1 = new Persona('Martin');
-const persona2 = new Persona('Caro');
+const persona1 = new Persona('Martin', '20-1231231-4');
+const persona2 = new Persona('Caro', '27-1231312-4');
 
 
 //instancio las instancias
+console.log('Persona 1');
 persona1.saludar();
 persona1.obtenerEspecie();
+persona1.getCuit();
+persona1.nombre = 'Martiiiin';
+persona1.saludar();
+console.log('Persona 2');
 persona2.saludar();
 persona2.obtenerEspecie();
+persona2.getCuit();
 
--- clases/clase-2/nullish-undefined.js
+++ clases/clase-2/nullish-undefined.js
@@ -3,3 +3,9 @@ const variableAsignada = prueba || 'sin valor'
 const nullish = prueba ?? 'sin valor'
 console.log(variableAsignada);
 console.log(nullish);
+
+const test1 = 0;
+const test2 = prueba || 'sin valor'
+const test3 = prueba ?? 'sin valor'
+console.log(test2);
+console.log(test3);
