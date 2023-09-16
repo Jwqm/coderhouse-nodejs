c44c0ab0af01859266cd36b07dd5439c91d61e25
Jose Quezada <jwqm.24@gmail.com>
2023-09-16 14:39:54 -0300

    Desafio Entregable 6

---
+++ desafios/desafio-entregable-6/src/middlewares/response.error.handler.js
@@ -0,0 +1,19 @@
+import { CustomError, NotFoundError } from '../errors/custom.error.js';
+
+const responseErrorHandler = (err, req, res, next) => {
+    console.error("Error occurred:", err);
+
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    if (err instanceof NotFoundError) {
+        return res.status(404).send({ status: 'error', code: err.code, message: err.message });
+    } else if (err instanceof CustomError) {
+        return res.status(500).send({ status: 'error', code: err.code, message: err.message });
+    }
+
+    return res.status(500).send({ status: 'error', code: 90000, message: "Internal server error" });
+};
+
+export default responseErrorHandler;
